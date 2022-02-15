7cd590a3fd64c3acc4892cfe409d8220745906c2
Osman <osman.gereyxanov@mail.ru>
2022-02-15 22:17:01 +0300

    less3

---
+++ Less_3/Task_3_1.py
@@ -0,0 +1,38 @@
+# №1
+def num_translate(language):
+    numerate = {
+        'one': 'один',
+        'two': 'два',
+        'three': 'три',
+        'four': 'четыре',
+        'fife': 'пять',
+        'six': 'шесть',
+        'seven': 'семь',
+        'eight': 'восемь',
+        'nine': 'девять'
+    }
+    return numerate.get(language)
+
+
+a = input("num_translate_adv:")
+print(num_translate(a))
+
+
+def num_translate_title(language):
+    numerate = {
+        'one': 'один',
+        'two': 'два',
+        'three': 'три',
+        'four': 'четыре',
+        'fife': 'пять',
+        'six': 'шесть',
+        'seven': 'семь',
+        'eight': 'восемь',
+        'nine': 'девять'
+    }
+    return numerate.get(language)
+
+
+a = input("num_translate_adv:")
+print(num_translate(a))
+
